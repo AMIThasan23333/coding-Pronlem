aab7b1a2911cd4f0cb9a4fd9429c13aee040289f
My Name <cpgsom819@gmail.com>
2023-02-10 22:50:29 +0800

    small changes

---
+++ largestNumber.js
@@ -0,0 +1,15 @@
+
+const max = Math.max(12,34,44);
+
+// console.log(max)
+
+const numbers = [12,78,99];
+
+const largest = Math.max(numbers);
+console.log(largest);
+
+const largest2 = Math.max(...numbers);
+
+
+console.log(largest2);
+
