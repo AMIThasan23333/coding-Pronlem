c0b9401690ab6714da89e660e9f5079015faf25a
My Name <cpgsom819@gmail.com>
2023-02-12 03:27:09 +0800

    nesting obj

--- nestedObject.js
+++ nestedObject.js
@@ -2,3 +2,21 @@
 const users = [{ id : 1, name : 'abul', job: 'doctor'}];
 
 console.log(users[0].name)
+
+
+
+const data = {
+
+    count : 4000,
+
+    data : [
+
+        {id : 1, name : 'babul', job : 'leader'},
+
+        {id : 1, name : 'babul', job : 'leader'},
+
+
+    ]
+}
+
+console.log(data.data[0].job);
