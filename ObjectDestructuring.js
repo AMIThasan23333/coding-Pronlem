2621ffe9db27fa55b0f786541f3b41dae9ca2a01
My Name <cpgsom819@gmail.com>
2023-02-12 00:30:33 +0800

    object destucturing

---
+++ ObjectDestructuring.js
@@ -0,0 +1,12 @@
+
+
+const fish = {
+
+    name : 'King Fish',
+    address :"Chadpur",
+    color: "yellow",
+}
+
+  const phone = fish.name;
+
+  console.log(phone);
---
+++ ReferenceType.js
@@ -0,0 +1,9 @@
+
+const numbers = [12,33,4,467];
+const numbers2 = numbers;
+
+numbers2.push(99);
+numbers.push(7);
+
+console.log(numbers);
+console.log(numbers2);
