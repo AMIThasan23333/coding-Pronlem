b69a71e79f85c312224316058cd57f25fd0dad91
My Name <cpgsom819@gmail.com>
2023-02-12 00:41:05 +0800

    another one

--- ObjectDestructuring.js
+++ ObjectDestructuring.js
@@ -8,5 +8,13 @@ const fish = {
 }
 
   const phone = fish.name;
+  const {address} = fish;
 
   console.log(phone);
+  console.log(address);
+
+  const {age, name} = { name : "aLMAS", age : 46, profession : 'makeup Artist'};
+  
+  console.log(age);
+
+
